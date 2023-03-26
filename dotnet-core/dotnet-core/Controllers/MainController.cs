745617905172ef9d0b26f322445d7257994f69a4
Ardi Jorganxhi <ardijorganxhi@eskisehir.edu.tr>
2023-03-26 17:33:48 +0300

    initial commit

---
+++ dotnet-core/dotnet-core/Controllers/MainController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace dotnet_core.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class MainController : ControllerBase
+    {
+        private readonly ILogger<MainController> _logger;
+
+        public MainController(ILogger<MainController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            double startTime = System.Diagnostics.Stopwatch.GetTimestamp();
+            long result = 1;
+            for (int i = 1; i < 1000; i++)
+            {
+                result += i;
+            }
+            double endTime = System.Diagnostics.Stopwatch.GetTimestamp();
+            double time = (endTime - startTime) / 100000;
+            return Ok(time);
+        }
+    }
+}
